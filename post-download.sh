958656c5c66872e8dee4cedde989470bc18671f7
Woo Jia Hao <woojiahao1234@gmail.com>
2025-02-16 18:07:53 +0800

    Add post-download script

--- post-download.sh
+++ post-download.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+touch file{1..100}.txt
+rm file77.txt
+
+git add file*.txt
+
+git commit -m "Change 1"
+
+echo "This is a change" >>file14.txt
+
+touch file77.txt
+
+# Remove permissions to edit these files for user
+chmod -w file{1..100}.txt
+
 first_commit_hash=$(git log --reverse --pretty=format:'%h' | head -n 1)
 if [ $(git tag -l "git-mastery-start-$first_commit_hash") ]; then
   git tag -d "git-mastery-start-$first_commit_hash"
